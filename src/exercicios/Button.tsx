83d6d0414c761c6ef7804d17e4881225ddc5323b
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2024-04-27 03:58:06 -0300

    commit

--- src/exercicios/Button.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Button = ({ total, setTotal }: {total: number, setTotal: React.Dispatch<React.SetStateAction<number>>}) => {
-  return (
-    <button onClick={() => setTotal((t) => t + 1)}>Incrementar {total}</button>
-  );
-};
-
-export default Button
